f56bb10045ca73ae19147a28ee752dad68b4b656
Javier Avalos <jaivalos18j2@gmail.com>
2022-08-03 22:17:37 -0600

    Creacion-de-la-clase-curso

--- Lab1-Relacion-Uno-muchos/src/main/java/com/mycompany/lab1/relacion/uno/muchos/Curso.java
+++ Lab1-Relacion-Uno-muchos/src/main/java/com/mycompany/lab1/relacion/uno/muchos/Curso.java
@@ -4,10 +4,39 @@
  */
 package com.mycompany.lab1.relacion.uno.muchos;
 
-/**
- *
- * @author Jaivalos
- */
 public class Curso {
+    
+    private String idCurso;
+    private int tamanio;
+    private Alumno alumnos[];
+    
+    //Constructor
+    public Curso(String idCurso, int tamanio){
+        this.idCurso = idCurso;
+        this.tamanio = tamanio;
+        this.alumnos = new Alumno[tamanio];
+    }
+    
+    //Get Set
+    public String getIdCurso(){
+        return idCurso;
+    }
+    public void setIdCurso(String idCurso){
+        this.idCurso = idCurso;
+    }
+    
+    public int getTamanio(){
+        return tamanio;
+    }
+    public void setTamanio(int tamanio){
+        this.tamanio = tamanio;
+    }
+    
+    public Alumno[] getAlumnos(){
+        return alumnos;
+    }
+    public void setAlumnos(Alumno[] alumnos){
+        this.alumnos = alumnos;
+    }
     
 }
